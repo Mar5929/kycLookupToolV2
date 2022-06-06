8730aac9add21d02a85be9ae8de81a85acb97b6f
Mike Rihm <michael@rihm.com>
2022-06-06 12:53:09 -0400

    removed .sfdx/* from the tracked git files

--- .sfdx/typings/lwc/apex/kycLookupLWCControllerV2.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/kycLookupLWCControllerV2.loadData" {
-  export default function loadData(param: {contentDocumentId: any, objAPIName: any, fieldAPIName: any, otherFieldAPIName: any, iDFieldAPIName: any}): Promise<any>;
-}
