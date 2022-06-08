fadc69b66f8522c74f91c873f647cdc8351c6a73
Mike Rihm <michael@rihm.com>
2022-06-08 19:25:29 -0400

    Working code base for export to csv feature

--- force-app/main/default/lwc/kycLookupToolV2/kycLookupToolV2.js
+++ force-app/main/default/lwc/kycLookupToolV2/kycLookupToolV2.js
@@ -117,4 +117,89 @@ export default class KycLookupToolV2 extends LightningElement {
         } ) 
 
     }
+    // this method validates the data and creates the csv file to download
+    downloadCSVFile() {   
+        this.isLoading = true;
+        let rowEnd = '\n';
+        let csvString = '';
+        // this set elminates the duplicates if have any duplicate keys
+        let rowData = new Set();
+
+        // getting keys from data
+        this.data.forEach(function (record) {
+            Object.keys(record).forEach(function (key) {
+                rowData.add(key);
+            });
+            window.console.log('rowData ===> '+rowData);
+        });
+
+        // Array.from() method returns an Array object from any object with a length property or an iterable object.
+        rowData = Array.from(rowData);
+        
+        // splitting using ','
+        csvString += rowData.join(',');
+        csvString += rowEnd;
+        
+        csvString = csvString.replace("otherFieldUrlWrapper,", "");
+        csvString = csvString.replace(",format", "");
+        window.console.log('csvString = '+ csvString);
+
+        // main for loop to get the data based on key value
+        for(let i=0; i < this.data.length; i++){
+            let colValue = 0;
+
+            // validating keys in data
+            for(let key in rowData) {
+                if(rowData.hasOwnProperty(key)) {
+                    // Key value 
+                    // Ex: Id, Name
+                    let rowKey = rowData[key];
+                    window.console.log('rowKey ===> '+ rowKey);
+                    if(colValue > 0 && rowKey !== 'otherFieldUrlWrapper'){
+                        csvString += ',';
+                    }
+
+                    if (rowKey === 'format') {
+                        // let value = '';
+                        // csvString += value;
+                        colValue++;
+                    }
+                    else if (rowKey === 'otherFieldUrlWrapper') {
+                        // let value = '';
+                        // csvString += value;
+                        colValue++;
+                    }
+                    else {
+                        let value = this.data[i][rowKey] === undefined ? '' : this.data[i][rowKey];
+                        window.console.log('value ===> '+ value);
+                        csvString += '"'+ value +'"';
+                        colValue++;
+                    }
+                }
+            }
+            
+            csvString += rowEnd;
+            csvString = csvString.replace("column3FieldWrapper", "Date of Birth");
+            csvString = csvString.replace("isKYCCompleteWrapper", "Is KYC Complete");
+            csvString = csvString.replace("objIdWrapper", "Relationship ID in FSC");
+            csvString = csvString.replace("otherFieldWrapper", "Name");
+            csvString = csvString.replace("uniqueFieldWrapper", "Tax Id");
+
+        }
+
+        // Creating anchor element to download
+        let downloadElement = document.createElement('a');
+
+        // This  encodeURI encodes special characters, except: , / ? : @ & = + $ # (Use encodeURIComponent() to encode these characters).
+        downloadElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvString);
+        downloadElement.target = '_self';
+        // CSV File Name
+        downloadElement.download = 'KYCLookupData.csv';
+        // below statement is required if you are using firefox browser
+        document.body.appendChild(downloadElement);
+        // click() Javascript function to download CSV file
+        downloadElement.click(); 
+        this.isLoading = false;
+    
+    }
 }
